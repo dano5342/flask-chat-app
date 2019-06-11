ebfd837a807f75b7807782f19cfb895dc96da774
Daniel Ellis <ellis.daniel0@gmail.com>
2019-06-11 10:08:12 +0000

    implemented basic views and routes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask Chat application
+
+The purpose of this project is to showcase a chat application using the Flask
+Framework. The purpose of the project is to take data from a URL and store it
+in a list
---
+++ run.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Main page with instructions """
+    return "To send a message us /USERNAME/MESSAGE"
+
+@app.route('/<username>')
+def user(username):
+    return "Hi " + username
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
