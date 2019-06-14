b40a11701069f852bdecc5ff3f148121de68c103
Daniel Ellis <ellis.daniel0@gmail.com>
2019-06-14 09:09:55 +0000

    changed a bad code on line 40-44

--- run.py
+++ run.py
@@ -1,31 +1,46 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "random123"
 messages = []
 
+
 def add_messages(username, message):
-    """add messages to the messages list """
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
 
 def get_all_messages():
-    """Get all of the messages and separate them with a ' break'"""
+    """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
-@app.route("/")
-def index():
-    """Main page with instructions """
-    return "To send a message us /USERNAME/MESSAGE"
 
-@app.route('/<username>')
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(session["username"])
+        
+    return render_template("index.html")
+
+
+@app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages)
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
-    """Create a new message and redirect the chat page """
+    """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
-    return redirect(username)
+    return redirect("/" + username)
+
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
